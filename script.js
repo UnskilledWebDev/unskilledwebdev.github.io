7703631c7b531296828af7ddc2fb5292e55ec99c
Lukas <gotobedkid@outlook.com>
2024-05-22 04:27:11 +0000

    Add test code to script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-/* there will be code here eventually.*/
+var bgColIn = document.getElementById("bgColor");
+var txtColIn = document.getElementById("txtColor");
+var bgColBtn = document.getElementById("bgColBtn")
+var txtColBtn = document.getElementById("txtColBtn");
+
+var bgBtnFunc = function() {
+    console.log(bgColIn.value);
+};
+var txtBtnFunc = function() {
+    console.log(txtColIn.value);
+}
+
+bgColBtn.addEventListener("click", bgBtnFunc);
+txtColBtn.addEventListener("click", txtBtnFunc);
